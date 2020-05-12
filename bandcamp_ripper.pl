83d4f9de659e07225621888a05e77692afe67fdd
sputnik <kss@disroot.org>
2020-05-12 19:05:35 +0300

    bandcamp_ripper.pl
    Change the way url is fed to script (used to be fed from stdin, now it's
    read from a parameter passed to script).
    Slight refactoring.

--- bandcamp_ripper.pl
+++ bandcamp_ripper.pl
@@ -3,7 +3,7 @@ use open ':std', ':encoding(UTF-8)';
 use strict;
 use utf8;
 use warnings;
-use Data::Dumper;
+#use Data::Dumper;
 use Encode qw(encode_utf8);
 use JSON qw(decode_json);
 use LWP::UserAgent;
@@ -12,33 +12,8 @@ use MP3::Tag;
 
 my $ua = LWP::UserAgent->new(timeout => 15);
 $ua->show_progress(0); #change to 1 for debugging
-get_album();
 
-sub get_album {
-    print "This script will download mp3s into album's subdirectory and fill in id3v2 tags.\n";
-    print "Enter album url: ";
-    my $url = <>;
-    my @track_pages = get_album_page($url);
-    my $length = @track_pages;
-    print "Found " . $length . " tracks. Downloading...\n";
-    foreach(@track_pages) {
-        get_track($_);
-    }
-    print "Done\n";
-}
-
-sub get_album_page {
-    my $response = $ua->get($_[0]);
-    $response->is_success ? my $html = $response->decoded_content : die "No response!";
-    my $dom = Mojo::DOM->new($html);
-    my $root = $dom->at("span[itemprop=byArtist]")->at("a")->attr("href");
-    my @track_pages;
-    for my $e ($dom->find('div[class=title]')->each) {
-        my $url = $root . $e->at("a[itemprop=url]")->attr("href");
-        push(@track_pages, $url);
-    }
-    return @track_pages;
-}
+main();
 
 sub fill_tags {
     my $mp3 = MP3::Tag->new($_[0]) or die "No file downloaded!";
@@ -50,17 +25,6 @@ sub fill_tags {
 		year => $info[4],
 	});
     $mp3->close();
-}
-
-sub get_track {
-    my @info = get_track_info($_[0]);
-    my $filename = $info[0] . " - " . $info[1] . ".mp3";
-    my $dir = $info[4] . " - " . $info[0] . " - " . $info[2];
-    my $path = $dir . "/" . $filename;
-    mkdir($dir);
-    $ua->mirror($info[3], $path);
-    fill_tags($path, \@info);
-    print "Saved to \"$path\"\n";
 }
 
 sub get_track_page {
@@ -92,5 +56,44 @@ sub get_track_info {
         }
     }
     return ($track_artist, $track_title, $track_album, $track_link, $track_year);
+}
+
+sub get_track {
+    my @info = get_track_info($_[0]);
+    my $filename = $info[0] . " - " . $info[1] . ".mp3";
+    my $dir = $info[4] . " - " . $info[0] . " - " . $info[2];
+    my $path = $dir . "/" . $filename;
+    mkdir($dir);
+    $ua->mirror($info[3], $path);
+    fill_tags($path, \@info);
+    print "Saved to \"$path\"\n";
+}
+
+sub get_album_page {
+    my $response = $ua->get($_[0]);
+    $response->is_success ? my $html = $response->decoded_content : die "No response!";
+    my $dom = Mojo::DOM->new($html);
+    my $root = $dom->at("span[itemprop=byArtist]")->at("a")->attr("href");
+    my @track_pages;
+    for my $e ($dom->find('div[class=title]')->each) {
+        my $url = $root . $e->at("a[itemprop=url]")->attr("href");
+        push(@track_pages, $url);
+    }
+    return @track_pages;
+}
+
+sub get_album {
+    my @track_pages = get_album_page($_[0]);
+    my $length = @track_pages;
+    print "Found " . $length . " tracks. Downloading...\n";
+    foreach(@track_pages) {
+        get_track($_);
+    }
+    print "Done\n";
+}
+
+sub main {
+    my $url = (exists($ARGV[0]) ? $ARGV[0] : "");
+    $url eq "" ? print "\nThis script will download mp3s into album's subdirectory and populate each file with id3v2 tags.\nPass bandcamp album link as a parameter in order to start the download:\n\n\$ ./script.pl https://80beats.bandcamp.com/album/the-bodega-tape\n\$ perl script.pl https://80beats.bandcamp.com/album/the-bodega-tape\n\nYou may also want to copy the script somewhere in your \$PATH to be able to execute it from any directory.\n\n" : get_album($url);
 }
 
